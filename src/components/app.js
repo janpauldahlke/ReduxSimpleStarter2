3e4c49ccbd1733252e11aee50873a39720442a4e
janpauldahlke <janpauldahlke@gmail.com>
2017-03-18 08:43:30 +0100

    scaffold react/redux

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+
+  render() {
+    return(
+      <div>entry point</div>
+    )
+  }
+}
+//exp
+export default App;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Animal Farm', author: 'G. Orwell'},
+    {title: '1984', author: 'G. Orwell'},
+    {title: 'Utopia', author: 'T. Morus'},
+    {title: 'brave new world', author: 'A. Huxley'}
+  ]
+}
